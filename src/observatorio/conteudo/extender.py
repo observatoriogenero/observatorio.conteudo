520a95d8b58a76bf74280a937020dc595251e832
Gustavo Lepri <gustavolepri@gmail.com>
2013-07-04 15:02:44 -0300

    atualizacao

--- src/observatorio/conteudo/extender.py
+++ src/observatorio/conteudo/extender.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from Products.Archetypes.public import BooleanField, LinesField
-from Products.Archetypes.public import BooleanWidget, InAndOutWidget
+from Products.Archetypes.public import BooleanField, StringField, LinesField
+from Products.Archetypes.public import BooleanWidget, StringWidget, InAndOutWidget
 
-from Products.ATContentTypes.interface import IATContentType
+from Products.ATContentTypes.interface import IATContentType, IATFile
 
 from zope.component import adapts
 from zope.interface import implements
 
 from archetypes.schemaextender.field import ExtensionField
-from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
-
-from zope.interface import Interface
+from archetypes.schemaextender.interfaces import ISchemaExtender
 
 from observatorio.conteudo.interfaces import IObservatorioConteudoLayer
-
-
-class IContentTypeExtender(Interface):
-    """
-    """
 
 
 class ContentTypeExtenderLinesField(ExtensionField, LinesField):
@@ -29,19 +22,11 @@ class ContentTypeExtenderBooleanField(ExtensionField, BooleanField):
     """ Um campo booleano simples """
 
 
-class ContentTypeExtender(object):
-    """
-    """
-
-    adapts(IATContentType)
-    implements(IBrowserLayerAwareExtender)
-    layer = IObservatorioConteudoLayer
-
-    fields = [
+fields = [
 
         ContentTypeExtenderLinesField(
             name="eixo",
-            index="KeywordIndex:brains",
+            index="KeywordIndex",
             widget=InAndOutWidget(
                 label="Eixo de Atuação",
             ),
@@ -52,7 +37,7 @@ class ContentTypeExtender(object):
 
         ContentTypeExtenderLinesField(
             name="area",
-            index="KeywordIndex:brains",
+            index="KeywordIndex",
             widget=InAndOutWidget(
                 label="Área Temática",
             ),
@@ -60,7 +45,33 @@ class ContentTypeExtender(object):
             multiValued=True,
             vocabulary_factory='observatorio.conteudo.areas_tematicas',
         ),
-    ]
+        ]
+
+
+class ContentTypeExtender(object):
+    """
+    """
+
+    adapts(IATContentType)
+    implements(ISchemaExtender)
+
+    fields = fields
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
+class FileExtender(object):
+    """
+    """
+
+    adapts(IATFile)
+    implements(ISchemaExtender)
+
+    fields = fields
 
     def __init__(self, context):
         self.context = context
